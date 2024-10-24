dd8e6925432af8899e2872964e9730ea0aaf5ab1
Sergey Potekhin <sergey@pimlico.io>
2024-10-24 13:39:09 +0400

    Add abi

---
+++ generate-abi.sh
@@ -0,0 +1,2 @@
+forge inspect src/MagicSpendLiquidityManager.sol:MagicSpendLiquidityManager abi > ./abi/MagicSpendLiquidityManager.abi.json
+forge inspect src/MagicSpendStakeManager.sol:MagicSpendStakeManager abi > ./abi/MagicSpendStakeManager.abi.json
