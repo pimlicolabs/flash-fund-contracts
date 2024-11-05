d84920d39a235a77ff471c4d42d784308459433f
Sergey Potekhin <sergey@pimlico.io>
2024-11-05 11:52:19 +0400

    Update deployment script

--- script/MagicSpend.s.sol
+++ script/MagicSpend.s.sol
@@ -1,34 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// import "forge-std/Script.sol";
-// import {MagicSpendStakeManager} from "./../src/MagicSpendStakeManager.sol";
-// import {MagicSpendWithdrawalManager} from "./../src/MagicSpendWithdrawalManager.sol";
-// import {ETH} from "./../src/base/Helpers.sol";
+import "forge-std/Script.sol";
+import {MagicSpendStakeManager} from "./../src/MagicSpendStakeManager.sol";
+import {MagicSpendWithdrawalManager} from "./../src/MagicSpendWithdrawalManager.sol";
+import {ETH} from "./../src/base/Helpers.sol";
 
-// contract MagicSpend_Deploy is Script {
-//     function setUp() public {}
+import {Upgrades} from "@openzeppelin-0.3.6/foundry-upgrades/Upgrades.sol";
 
-//     function run() public returns (address _stakeManager, address _withdrawalManager) {
-//         address deployer = vm.rememberKey(vm.envUint("DEPLOYER"));
-//         address owner = vm.rememberKey(vm.envUint("OWNER"));
-//         address signer = vm.rememberKey(vm.envUint("SIGNER"));
-//         address alice = vm.rememberKey(vm.envUint("ALICE"));
+contract MagicSpend_Deploy is Script {
+    function setUp() public {}
 
-//         bytes32 salt = vm.envBytes32("SALT");
+    function run() public returns (address _stakeManager, address _withdrawalManager) {
+        address deployer = vm.rememberKey(vm.envUint("DEPLOYER"));
+        address owner = vm.rememberKey(vm.envUint("OWNER"));
+        address signer = vm.rememberKey(vm.envUint("SIGNER"));
+        address alice = vm.rememberKey(vm.envUint("ALICE"));
 
-//         vm.startBroadcast(deployer);
-//         MagicSpendStakeManager stakeManager = new MagicSpendStakeManager{salt: salt}(owner);
+        // bytes32 salt = vm.envBytes32("SALT");
 
-//         MagicSpendWithdrawalManager withdrawalManager = new MagicSpendWithdrawalManager{salt: salt}(owner, signer);
+        vm.startBroadcast(deployer);
+        address proxyStakeManager = Upgrades.deployTransparentProxy(
+            "MagicSpendStakeManager.sol", owner, abi.encodeCall(MagicSpendStakeManager.initialize, (owner))
+        );
 
-//         withdrawalManager.addLiquidity{value: 0.01 ether}(ETH, 0.01 ether);
-//         vm.stopBroadcast();
+        MagicSpendStakeManager stakeManager = MagicSpendStakeManager(payable(proxyStakeManager));
 
-//         vm.startBroadcast(alice);
-//         stakeManager.addStake{value: 0.01 ether}(ETH, 0.01 ether, 86400);
-//         vm.stopBroadcast();
+        address proxyWithdrawalManager = Upgrades.deployTransparentProxy(
+            "MagicSpendWithdrawalManager.sol",
+            owner,
+            abi.encodeCall(MagicSpendWithdrawalManager.initialize, (owner, signer))
+        );
 
-//         return (address(stakeManager), address(withdrawalManager));
-//     }
-// }
+        MagicSpendWithdrawalManager withdrawalManager = MagicSpendWithdrawalManager(payable(proxyWithdrawalManager));
+
+        withdrawalManager.addLiquidity{value: 0.01 ether}(ETH, 0.01 ether);
+        vm.stopBroadcast();
+
+        vm.startBroadcast(alice);
+        stakeManager.addStake{value: 0.01 ether}(ETH, 0.01 ether, 86400);
+        vm.stopBroadcast();
+
+        return (address(stakeManager), address(withdrawalManager));
+    }
+}
